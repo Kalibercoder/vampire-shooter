101df87ac0607ef341627cf228922f20438a89b7
Theo Archer <117527935+Kalibercoder@users.noreply.github.com>
2024-11-15 17:08:03 +0100

    Changed monster to a ball so I dont have to deal with the enemy rotation being correct to the image

--- enemy.py
+++ enemy.py
@@ -8,11 +8,12 @@ from player import *
 class Enemy(CircleShape):
     def __init__(self, x, y, radius):
         super().__init__(x, y, radius)
-        self.image = pygame.image.load("imgs/dog2.png").convert_alpha()  
+        self.image = pygame.image.load("imgs/ballmonster.png").convert_alpha()  
         self.image = pygame.transform.scale(self.image, (100, 100))  
         self.original_image = self.image
         self.rect = self.image.get_rect(center=(x, y))  
         self.speed = ENEMY_SPEED
+        self.velocity = pygame.Vector2(0, 0)
 
     def chase(self, Player):
         direction = Player - self.position
@@ -27,6 +28,11 @@ class Enemy(CircleShape):
     def update(self, dt):
         self.position += self.velocity * dt  
         self.rect.center = self.position.xy
+
+        if self.velocity.length() > 0:  
+            angle = -self.velocity.angle_to(pygame.Vector2(0, 1))  
+            self.image = pygame.transform.rotate(self.original_image, angle)
+            self.rect = self.image.get_rect(center=self.position.xy)
 
     def split(self):
         self.kill() 
